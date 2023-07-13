0ec32e112e428525ed4e269f93b330aac0bd8135
Rick_Paramanik <rick.paramanik@faceacademy.in>
2023-07-13 22:08:57 +0530

    arrow added at productCategorySlider

--- client/src/components/productCategorySlider/ProductCategorySlider.jsx
+++ client/src/components/productCategorySlider/ProductCategorySlider.jsx
@@ -1,18 +1,28 @@
 import { Container, Typography } from "@mui/material";
 import { ProductCard } from "./productCard/ProductCard";
-import { SliderBox, StyledBox } from "./style";
+import { LeftIcon, RightIcon, SliderBox, StyledBox } from "./style";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getAllProducts } from "../../services/product.service";
+
+import { Box } from "@mui/system";
 
 export const ProductCardSlider = ({ categoryId, name }) => {
     const [allProducts, setAllProducts] = useState([]);
+    const slideRef = useRef(null);
     useEffect(() => {
         getAllProducts().then((data) => {
             setAllProducts(data);
         });
     }, []);
-    
+
+    const slideHandler = (dir) => {
+        const slideLength = slideRef.current.clientWidth / 2;
+        dir == "l"
+            ? (slideRef.current.scrollLeft -= slideLength)
+            : (slideRef.current.scrollLeft += slideLength);
+    };
+
     return (
         <>
             <Container sx={{ padding: "18px 0px" }}>
@@ -23,22 +33,31 @@ export const ProductCardSlider = ({ categoryId, name }) => {
                 >
                     {name}
                 </Typography>
-                <SliderBox>
-                    <StyledBox>
-                        {allProducts
-                            .filter(
-                                (item) => item.sub_category_id === categoryId
-                            )
-                            .map((item) => {
-                                return (
-                                    <ProductCard
-                                        {...item}
-                                        key={Math.random()}
-                                    />
-                                );
-                            })}
-                    </StyledBox>
-                </SliderBox>
+                <Box
+                    sx={{
+                        position: "relative",
+                    }}
+                >
+                    <LeftIcon onClick={() => slideHandler("l")} />
+                    <SliderBox ref={slideRef}>
+                        <StyledBox>
+                            {allProducts
+                                .filter(
+                                    (item) =>
+                                        item.sub_category_id === categoryId
+                                )
+                                .map((item) => {
+                                    return (
+                                        <ProductCard
+                                            {...item}
+                                            key={Math.random()}
+                                        />
+                                    );
+                                })}
+                        </StyledBox>
+                    </SliderBox>
+                    <RightIcon onClick={() => slideHandler("r")} />
+                </Box>
             </Container>
         </>
     );
--- client/src/components/productCategorySlider/style.js
+++ client/src/components/productCategorySlider/style.js
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
+import NavigateBeforeOutlinedIcon from "@mui/icons-material/NavigateBeforeOutlined";
+import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
 
 export const StyledBox = styled(Box)(({ theme }) => ({
     display: "flex",
@@ -9,6 +11,7 @@ export const StyledBox = styled(Box)(({ theme }) => ({
 export const SliderBox = styled(Box)(({ theme }) => ({
     position: "relative",
     overflow: "scroll",
+    scrollBehavior: "smooth",
     "&::-webkit-scrollbar": {
         width: "0.4em",
         height: "3px",
@@ -19,5 +22,35 @@ export const SliderBox = styled(Box)(({ theme }) => ({
     },
     "&::-webkit-scrollbar-thumb": {
         backgroundColor: "rgba(0,0,0,.1)",
+    },
+}));
+
+const commonArrowStyle = {
+    fontSize: "72px",
+    zIndex: 1,
+    opacity: 0.3,
+    borderRadius: "50%",
+    cursor: "pointer",
+    transition: "all .3s ease",
+};
+
+export const LeftIcon = styled(NavigateBeforeOutlinedIcon)(({ theme }) => ({
+    ...commonArrowStyle,
+    position: "absolute",
+    top: "50%",
+    left: "1%",
+    transform: "translate(0%,-50%)",
+    "&:hover": {
+        backgroundColor: "gray",
+    },
+}));
+export const RightIcon = styled(NavigateNextOutlinedIcon)(({ theme }) => ({
+    ...commonArrowStyle,
+    position: "absolute",
+    top: "50%",
+    right: "1%",
+    transform: "translate(0%,-50%)",
+    "&:hover": {
+        backgroundColor: "gray",
     },
 }));
