d9f77138c0a1dd2726a6b3144e5bfeb50afc8e24
Rick_Paramanik <rick.paramanik@faceacademy.in>
2023-07-15 09:39:32 +0530

    user model changed

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -6,10 +6,6 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
-    image: {
-        type: String,
-        required: true,
-    },
     email: {
         type: String,
         required: true,
